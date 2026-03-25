0f7e8ea64e9ff749788aa35ff92f43f0db304eb7
kaniop <kitsanapon_pin@hotmail.com>
2026-03-25 22:22:21 +0700

    fix contactme page component

--- app/contactme/page.tsx
+++ app/contactme/page.tsx
@@ -1,9 +1,7 @@
-// app/contactme/page.tsx
-
 export default function ContactMe() {
   return (
-    <div className="flex flex-col justify-center w-full h-full pl-10 md:pl-20 pr-10">
-      <h1 className="text-4xl text-white">Contact Me (Coming Soon)</h1>
+    <div className="text-white p-20 text-4xl">
+      Contact Me Page
     </div>
   );
 }
