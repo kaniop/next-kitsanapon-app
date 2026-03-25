71023d62ffaeeaa9433fbfc720aaa6fc54c9d82d
kaniop <kitsanapon_pin@hotmail.com>
2026-03-25 22:16:01 +0700

    fix contactme prerender error

---
+++ app/contactme/page.tsx
@@ -0,0 +1,9 @@
+// app/contactme/page.tsx
+
+export default function ContactMe() {
+  return (
+    <div className="flex flex-col justify-center w-full h-full pl-10 md:pl-20 pr-10">
+      <h1 className="text-4xl text-white">Contact Me (Coming Soon)</h1>
+    </div>
+  );
+}
